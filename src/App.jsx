5260b58a2906500e0e20521c2983112e3e027a06
AhmedAttia0 <AhmedAboulenein0147@gmail.com>
2026-04-13 21:14:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavbarHome from "./components/navbar/NavbarHome";
+
+function App() {
+  return (
+    <div>
+      <NavbarHome />
+    </div>
+  );
+}
+
+export default App;
