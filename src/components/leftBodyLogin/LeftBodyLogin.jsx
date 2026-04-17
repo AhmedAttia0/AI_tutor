68ec56e9ab22281a479f9f271705dc211dcf4e77
AhmedAttia0 <AhmedAboulenein0147@gmail.com>
2026-04-17 13:18:11 +0200

    final login

--- src/components/leftBodyLogin/LeftBodyLogin.jsx
+++ src/components/leftBodyLogin/LeftBodyLogin.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import robot from "../../assets/image2.png";
+import icon1 from "../../assets/icon1.png";
+import icon2 from "../../assets/icon2.png";
+import icon3 from "../../assets/icon3.png";
+import { GoDotFill } from "react-icons/go";
 
 const LeftBodyLogin = () => {
   return (
-    <div>LeftBodyLogin</div>
-  )
-}
+    <div className="">
+      <div className="relative rounded-3xl bg-[#3656B3] p-8 overflow-hidden">
+        <div className="absolute -top-0 -left-0 h-[21.75rem] w-[21.75rem] rounded-full bg-[#60A5FA33]"></div>
 
-export default LeftBodyLogin
+        <div className="relative z-10 text-center flex flex-col items-center gap-1">
+          <div className="mx-auto h-[12rem] w-[12rem] ">
+            {" "}
+            <img src={robot} alt="" />
+          </div>
+
+          <div className="mb-5">
+            <h2 className="mt-6 text-3xl font-bold text-white">
+              Meet Your AI Mentor
+            </h2>
+            <p className="mt-2 text-white/80">
+              I'm CogniBot, your intelligent learning companion
+            </p>
+          </div>
+          <div className="bg-[#FFFFFF0D] p-5 rounded-3xl flex flex-col gap-5">
+            <div className="flex flex-row items-center justify-start gap-3">
+              <div>
+                <img src={icon1} className="w-10 h-10" alt="" />
+              </div>
+              <div className="flex flex-col items-start">
+                <p className="text-base font-medium text-white">
+                  Advanced Learning Intelligence
+                </p>
+                <p className="text-sm font-normal text-[#BFDBFE]">
+                  I adapt to your learning style and pace in real-time
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-start gap-3">
+              <div>
+                <img src={icon2} className="w-10 h-10" alt="" />
+              </div>
+              <div className="flex flex-col items-start">
+                <p className="text-base font-medium text-white">
+                  Natural Conversation
+                </p>
+                <p className="text-sm font-normal text-[#BFDBFE]">
+                  Ask me anything, anytime - I understand context
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-start gap-3">
+              <div>
+                <img src={icon3} className="w-10 h-10" alt="" />
+              </div>
+              <div className="flex flex-col items-start">
+                <p className="text-base font-medium text-white">
+                  Personalized Guidance
+                </p>
+                <p className="text-sm font-normal text-[#BFDBFE]">
+                  I track your progress and fill knowledge gaps
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row items-center  ">
+            <GoDotFill className="text-[#22C55E]" />
+            <p className="font-normal text-[#DBEAFE] text-sm">
+              Online & Ready to Help 24/7
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftBodyLogin;
--- src/components/rightBodyLogin/RightBodyLogin.jsx
+++ src/components/rightBodyLogin/RightBodyLogin.jsx
@@ -1,19 +1,57 @@
 import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
 import { MdEmail } from "react-icons/md";
-import { FaLock } from "react-icons/fa";
+import { FaEyeSlash, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { useState } from "react";
+import { FaGoogle } from "react-icons/fa";
+import { FaMicrosoft } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
+import { FaShieldAlt } from "react-icons/fa";
 
 const RightBodyLogin = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    remember: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Form Data:", formData);
+
+    //send data to backend
+  };
+
+  const handleSocialLogin = (provider) => {
+    console.log(`${provider} login clicked`);
+
+    alert(`${provider} login is coming soon`);
+
+    // later:
+    // window.location.href = `/auth/${provider.toLowerCase()}`
+  };
   return (
-    <div>
-      <Card className="max-w-sm">
+    <div className="w-full min-w-full">
+      <Card className="">
         <div>
           <h1 className="font-bold text-[#1E3A8A] text-2xl">Welcome Back</h1>
           <p className="font-normal text-base">
             Sign in to continue your learning journey with CogniBot
           </p>
         </div>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div>
             <div className="mb-2 block ">
               <Label className="text-[#1E3A8A] font-semibold" htmlFor="email1">
@@ -26,38 +64,111 @@ const RightBodyLogin = () => {
               placeholder="your.email@example.com"
               required
               icon={MdEmail}
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
             />
           </div>
-          <div>
-            <div className="mb-2 block">
-              <Label
-                className="text-[#1E3A8A] font-semibold"
-                htmlFor="password1"
-              >
-                password
-              </Label>
-            </div>
+
+          <div className="mb-2 block ">
+            <Label className="text-[#1E3A8A] font-semibold" htmlFor="password1">
+              password
+            </Label>
+          </div>
+          <div className="relative">
             <TextInput
               id="password1"
-              type="password"
+              type={showPassword ? "text" : "password"}
               icon={FaLock}
               required
               placeholder="Enter your password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
             />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500 hover:text-[#1E3A8A] transition-colors"
+            >
+              {" "}
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
           </div>
           <div className="flex flex-row justify-between gap-2">
             <div>
               {" "}
-              <Checkbox id="remember" />
+              <Checkbox
+                id="remember"
+                name="remember"
+                checked={formData.remember}
+                onChange={handleChange}
+              />
               <Label htmlFor="remember">Remember me</Label>
             </div>
-           <div>
-            <Link to='#' className="font-semibold text-[#3B82F6] text-sm">Forgot Password?</Link>
-           </div>
+            <div>
+              <Link to="#" className="font-semibold text-[#3B82F6] text-sm">
+                Forgot Password?
+              </Link>
+            </div>
           </div>
-          <Button className="bg-gradient-to-r from-[#1E3A8A] to-[#3B82F6] text-lg font-semibold" type="submit">Sign in</Button>
+          <Button
+            className="bg-gradient-to-r from-[#1E3A8A] to-[#3B82F6] text-lg font-semibold"
+            type="submit"
+          >
+            Sign in
+          </Button>
+          <div className="flex items-center my-2">
+            <hr className="flex-grow border-gray-300" />
+            <span className="px-2 text-sm text-gray-500">Or continue with</span>
+            <hr className="flex-grow border-gray-300" />
+          </div>
+          <div className="flex flex-row justify-between gap-2">
+            <div className="w-[30%]">
+              <Button
+                className="w-full text-xl text-[#4B5563]"
+                color="alternative"
+                onClick={() => handleSocialLogin("Google")}
+              >
+                <FaGoogle />
+              </Button>
+            </div>
+            <div className="w-[30%] ">
+              <Button
+                className="w-full text-xl text-[#4B5563]"
+                color="alternative"
+                onClick={() => handleSocialLogin("Microsoft")}
+              >
+                <FaMicrosoft />
+              </Button>
+            </div>
+            <div className="w-[30%]">
+              <Button
+                className="w-full text-xl text-[#4B5563]"
+                color="alternative"
+                onClick={() => handleSocialLogin("Apple")}
+              >
+                <FaApple />
+              </Button>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <p className="text-base">
+              Don't have an account?{" "}
+              <Link to="#" className="text-[#3B82F6] font-normal text-base">
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </form>
       </Card>
+      <div className="my-3 flex flex-row items-center justify-center gap-1 bg-[#FFFFFF1A] rounded-2xl text-sm font-normal w-fit px-3 py-1 mx-auto">
+        <div className="text-[#60A5FA] ">
+          <FaShieldAlt />
+        </div>
+
+        <p className="text-white">Your data is encrypted and secure</p>
+      </div>
     </div>
   );
 };
